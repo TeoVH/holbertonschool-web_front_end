2d41308df52baa8f25d2e31377958e6c9712efe7
TeoVH <villadamateo64@gmail.com>
2023-01-24 17:14:01 -0500

    Lexical scoping and welcome message

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,9 @@
+function welcome(firstName, lastName) {
+    let fullName = `${firstName} ${lastName}`
+
+    function displayFullname(){
+        alert(`Welcome ${fullName}!`)
+    }
+
+    displayFullname()
+}
